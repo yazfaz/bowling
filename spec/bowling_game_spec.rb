1a707f43c53ea8bea360139b36fb367dd328d6cf
Yasmin Fazelin <yfazelinia@gmail.com>
2013-09-12 14:05:27 -0500

    Add let method.

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -4,9 +4,9 @@ require 'rspec'
 require_relative '../bowling_game'
 
 describe BowlingGame do
+  let(:game) {BowlingGame.new}
   describe '.initialize' do
     it 'sets @rolls to an empty array' do
-      game= BowlingGame.new
 
       expect(game.rolls).to eq([])
     end
@@ -14,14 +14,12 @@ describe BowlingGame do
 
   describe '#score' do
     it 'scores t he game as 0 when only gutter balls are thrown' do
-      game= BowlingGame.new
       20.times { game.roll(0)}
 
       expect(game.score).to eq(0)
     end
 
     it 'scores the game as 20 when one pin is knocked down on each roll' do
-      game=BowlingGame.new
       20.times {game.roll(1)}
 
       expect(game.score).to eq(20)
