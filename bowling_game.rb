39ee5ca8d2613c6e0df75011c0341459c5af6428
Yasmin Fazelin <yfazelinia@gmail.com>
2013-09-12 14:32:53 -0500

    Add strike logic to score method

--- bowling_game.rb
+++ bowling_game.rb
@@ -4,9 +4,11 @@ class BowlingGame
     @rolls=[]
   end
 
-# Record a roll in the game
-# pins - The integer number of pins knocked down in this roll. 
-# Returns nothing 
+  # Record a roll in the game
+  #
+  # pins - The integer number of pins knocked down in this roll. 
+  #
+  # Returns nothing 
   def roll(pins)
     @rolls.push(pins)
   end
@@ -18,14 +20,18 @@ class BowlingGame
       roll        = @rolls[current_roll]
       next_roll   = @rolls[current_roll + 1]
     
-      if roll + next_roll == 10
+      if roll == 10
+        total_score += 10 + next_roll + @rolls[current_roll + 2]
+        current_roll += 1
+      elsif roll + next_roll == 10
         total_score += 10 + @rolls[current_roll + 2]
+        current_roll += 2
       else
         total_score += roll + next_roll
+        current_roll += 2
       end
-
-      current_roll +=2
     end
+
     return total_score
   end
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -28,12 +28,24 @@ describe BowlingGame do
         expect(game.score).to eq(20)
       end
     end
+
     context 'when a spare is thrown' do
       it 'records the frame score as 10 plus the number of pins knocked down in the next roll' do
         3.times { game.roll(5)}
         17.times { game.roll(0)}
 
         expect(game.score).to eq(20)
+      end
+    end
+    
+    context 'when strike is thrown' do
+      it 'records the frame score as 10 plus the total number of pins knocked down in the next two rolls' do
+        game.roll(10)
+        game.roll(7)
+        game.roll(1)
+        16.times { game.roll(0) }
+
+        expect(game.score).to eq(26)
       end
     end
   end
