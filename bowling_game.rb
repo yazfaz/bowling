b8af9ed2bdf2ecad844e6a92edc1c8182556e408
MakerSquare Student <students@themakersquare.com>
2013-09-12 15:19:22 -0500

    Refactor score method

--- bowling_game.rb
+++ bowling_game.rb
@@ -1,7 +1,9 @@
 class BowlingGame
-  attr_accessor :rolls
+  attr_accessor :rolls, :total_score, :current_roll
   def initialize
     @rolls=[]
+    @total_score=0
+    @current_roll=0
   end
 
   # Record a roll in the game
@@ -12,26 +14,61 @@ class BowlingGame
   def roll(pins)
     @rolls.push(pins)
   end
-
+ 
+  #Returns the Integer score for this game. Expects to be run after all rolls for the game have been recorded.
   def score
-    total_score = 0
-    current_roll = 0
-    while current_roll < @rolls.size - 1
-      roll        = @rolls[current_roll]
-      next_roll   = @rolls[current_roll + 1]
+    while @current_roll < @rolls.size - 1
+      init_roll
     
-      if roll == 10
-        total_score += 10 + next_roll + @rolls[current_roll + 2]
-        current_roll += 1
-      elsif roll + next_roll == 10
-        total_score += 10 + @rolls[current_roll + 2]
-        current_roll += 2
+      if strike?
+        score_strike
+      elsif spare?
+        score_spare
       else
-        total_score += roll + next_roll
-        current_roll += 2
+        score_normal
       end
     end
 
-    return total_score
+    return @total_score
   end
+
+  private
+  #Initialize values for roll and next_roll.
+  # 
+  # Returns nothing. 
+  def init_roll
+    @roll      = @rolls[@current_roll]
+    @next_roll = @rolls[@current_roll + 1]
+  end
+    
+  # Returns true if the current roll is a strike, false otherwise. 
+  def strike?
+    @roll == 10
+  end  
+  
+  # Scores a strike frame, and adds it to the total score for the game.
+  # 
+  # Returns nothing. 
+  def score_strike
+    @total_score += 10 + @next_roll + @rolls[@current_roll +2]
+    @current_roll += 1     
+  end
+  # Returns true if the current roll is a spare, false otherwise.
+  def spare?
+    @roll + @next_roll == 10
+  end
+
+# Scores a spare frame, and adds it to the total score for the game.
+# 
+# Returns nothing. 
+  def score_spare
+    @total_score += 10 + @rolls[@current_roll + 2]
+        @current_roll += 2
+  end
+
+  def score_normal
+    @total_score += @roll + @next_roll
+        @current_roll += 2
+  end   
+
 end
