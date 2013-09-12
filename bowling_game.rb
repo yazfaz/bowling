0076dbeb974340dd9a8e2de8976269dc09b59a51
Yasmin Fazelin <yfazelinia@gmail.com>
2013-09-12 14:23:49 -0500

    Add score rolls for spares

--- bowling_game.rb
+++ bowling_game.rb
@@ -12,6 +12,20 @@ class BowlingGame
   end
 
   def score
-    @rolls.reduce(:+)
+    total_score = 0
+    current_roll = 0
+    while current_roll < @rolls.size - 1
+      roll        = @rolls[current_roll]
+      next_roll   = @rolls[current_roll + 1]
+    
+      if roll + next_roll == 10
+        total_score += 10 + @rolls[current_roll + 2]
+      else
+        total_score += roll + next_roll
+      end
+
+      current_roll +=2
+    end
+    return total_score
   end
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -13,16 +13,28 @@ describe BowlingGame do
   end
 
   describe '#score' do
-    it 'scores t he game as 0 when only gutter balls are thrown' do
-      20.times { game.roll(0)}
+    context 'when only gutter balls are thrown' do
+      it 'scores the game as 0 when only gutter balls are thrown' do
+        20.times { game.roll(0)}
 
-      expect(game.score).to eq(0)
+        expect(game.score).to eq(0)
+      end
     end
 
-    it 'scores the game as 20 when one pin is knocked down on each roll' do
-      20.times {game.roll(1)}
+    context 'when one pin is knocked down on each roll' do
+      it 'scores the game as 20 when one pin is knocked down on each roll' do
+        20.times {game.roll(1)}
 
-      expect(game.score).to eq(20)
+        expect(game.score).to eq(20)
+      end
+    end
+    context 'when a spare is thrown' do
+      it 'records the frame score as 10 plus the number of pins knocked down in the next roll' do
+        3.times { game.roll(5)}
+        17.times { game.roll(0)}
+
+        expect(game.score).to eq(20)
+      end
     end
   end
 end
